da6b1f964c38e5171b32dc00b6a267771daa1d80
Oleg Ganus <oleg110770@gmail.com>
2025-04-08 17:41:14 +0300

    edit task

--- js/task-4.js
+++ js/task-4.js
@@ -1,16 +1,16 @@
 function getShippingCost(country) {
     switch (country) {
         case "China":
-            return `Shipping to ${country} will cost 100 кредитів credits`
+            return `Shipping to ${country} will cost 100 credits`
             
         case "Chile":
-            return `Shipping to ${country} will cost 250 кредитів credits`
+            return `Shipping to ${country} will cost 250 credits`
             break;
         case "Australia":
-            return `Shipping to ${country} will cost 170 кредитів credits`
+            return `Shipping to ${country} will cost 170 credits`
             break;
         case "Jamaica":
-            return `Shipping to ${country} will cost 120 кредитів credits`
+            return `Shipping to ${country} will cost 120 credits`
             break;
         default:
             return `Sorry, there is no delivery to your country`;
